2edb7f3928dab9749dc6c1d4f98057fd9005a293
Marcos Pagliaruzza <ml.paglia96@gmail.com>
2024-02-29 11:10:00 +0100

    V 1.0

--- src/main/java/N1EX8/App.java
+++ src/main/java/N1EX8/App.java
@@ -6,15 +6,7 @@ public class App {
 
     public static void runProgram(){
 
-        StringBuilder reversed = new StringBuilder();
-        ReverseString stringReverse = (String message) -> {
-
-            for(int i = message.length() - 1; i >= 0; i--) {
-                reversed.append(message.charAt(i));
-            }
-            return reversed.toString();
-        };
-
+        ReverseString stringReverse = (String message) -> new StringBuilder(message).reverse().toString();
         System.out.println("String reverse: " + stringReverse.reverse("My name is Marcos"));
 
     }
